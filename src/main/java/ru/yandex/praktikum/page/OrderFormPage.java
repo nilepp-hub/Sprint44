be93eafc89a692000de052d7ad3c8d0e2b90edd7
nilepp-hub <nilepp@yahoo.com>
2023-02-12 17:47:02 +0300

    Фиксы по коментариям

--- src/main/java/ru/yandex/praktikum/page/OrderFormPage.java
+++ src/main/java/ru/yandex/praktikum/page/OrderFormPage.java
@@ -1,53 +1,59 @@
-package Page;
-
+package ru.yandex.praktikum.page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 
 public class OrderFormPage {
-    private static final By NAME = By.xpath("//input[@placeholder=\"* Имя\"]");
-    private static final By FAM = By.cssSelector("#root > div > div.Order_Content__bmtHS > div.Order_Form__17u6u > div:nth-child(2) > input");
-    private static final By ADRES = By.cssSelector("#root > div > div.Order_Content__bmtHS > div.Order_Form__17u6u > div:nth-child(3) > input");
-    private static final By METRO = By.cssSelector("#root > div > div.Order_Content__bmtHS > div.Order_Form__17u6u > div:nth-child(4) > div > div > input");
-    private static final By STATION = By.className("select-search__row");
-    private static final By PHONE = By.xpath("//input[@placeholder=\"* Телефон: на него позвонит курьер\"]");
-    private static final By DALEE = By.xpath("//*[@id=\"root\"]/div/div[2]/div[3]/button");
-    private static WebDriver driver;
+    private final By NAME = By.xpath("//input[@placeholder=\"* Имя\"]");
+    private final By FAM = By.xpath("//input[@placeholder=\"* Фамилия\"]");
+    private final By ADRES = By.xpath("//input[@placeholder=\"* Адрес: куда привезти заказ\"]");
+    private final By METRO = By.xpath("//input[@placeholder=\"* Станция метро\"]");
+    private final By STATION = By.className("select-search__row");
+    private final By PHONE = By.xpath("//input[@placeholder=\"* Телефон: на него позвонит курьер\"]");
+    private final By DALEE = By.xpath("//button[@class=\"Button_Button__ra12g Button_Middle__1CSJM\"][text()=\"Далее\"]");
+    private WebDriver driver;
     public OrderFormPage(WebDriver driver) {
-
         this.driver = driver;
     }
-
-
-    public static void fillOrderUpZakaz() {
+    public void fillOrderUpZakaz() {
         driver.findElement(NAME).sendKeys("Борис");
         driver.findElement(FAM).sendKeys("Борисов");
         driver.findElement(ADRES).sendKeys("Борисова 1");
         driver.findElement(PHONE).sendKeys("+79899507744");
     }
-    public static void fillOrderDownZakaz() {
+    public void fillOrderDownZakaz() {
         driver.findElement(NAME).sendKeys("Anna");
         driver.findElement(FAM).sendKeys("Annovna");
         driver.findElement(ADRES).sendKeys("Annovnikov 1");
         driver.findElement(PHONE).sendKeys("+798995077444");
     }
-    public  static void vvodMetro1() {
+    public void vvodMetro1() {
         driver.findElement(METRO).clear();
         driver.findElement(METRO).sendKeys("цветной");
         new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(STATION));
         driver.findElement(STATION).click();
     }
-    public  static void vvodMetro2() {
+    public void vvodMetro2() {
         driver.findElement(METRO).clear();
         driver.findElement(METRO).sendKeys("черкизовская");
         new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(STATION));
         driver.findElement(STATION).click();
     }
-    public  static void clickDalee() {
+    public void clickDalee() {
         new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(DALEE));
-      driver.findElement(DALEE).click();
+        driver.findElement(DALEE).click();
+    }
+    //шаг заполнения формы для позитивного прохода, кнопка заказа в шапке.
+    public void upZakazFormPozitiv() {
+        fillOrderUpZakaz();
+        vvodMetro1();
+        clickDalee();
+    }
+    public void downZakazFormNegativ() {
+        fillOrderDownZakaz();
+        vvodMetro2();
+        clickDalee();
     }
 }
--- src/main/java/ru/yandex/praktikum/page/RoadMapPage.java
+++ src/main/java/ru/yandex/praktikum/page/RoadMapPage.java
@@ -1,21 +1,17 @@
-package Page;
-
+package ru.yandex.praktikum.page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 
 public class RoadMapPage {
-    private static final By DOWN_ZAKAZ_BUTTON = By.cssSelector("#root > div > div.Home_HomePage__ZXKIX > div.Home_ThirdPart__LSTEE > div.Home_RoadMap__2tal_ > div.Home_FinishButton__1_cWm > button");
-
-    private static WebDriver driver;
+    private final By DOWN_ZAKAZ_BUTTON = By.cssSelector("div.Home_FinishButton__1_cWm > button");
+    private WebDriver driver;
     public RoadMapPage(WebDriver driver) {
-
         this.driver = driver;
     }
-    public static void downZakazClick() {
+    public void downZakazClick() {
         new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(DOWN_ZAKAZ_BUTTON));
         driver.findElement(DOWN_ZAKAZ_BUTTON).click();
     }
